c5b820c5360fb452629eaf5240cecd1587aba3da
Allar Klein <allarklein@gmail.com>
2025-11-07 18:53:19 +0200

    chose: basic project setup

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Env for CPP and Python assignment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+
+            cmake
+            gcc
+            gdb
+            clang
+            python313
+            python313Packages.flask
+            python313Packages.pip
+          ];
+
+          shellHook = ''
+            echo "Environment loaded"
+          '';
+        };
+      }
+    );
+}
